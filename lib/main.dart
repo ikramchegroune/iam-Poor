8731e5d07f1d80298fa888aaeee5c16e31e1a5de
ikramchegroune <chegrouneusthb@gmail.com>
2023-03-02 19:45:10 +0100

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'I am Poor',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.orange[100],
+      appBar: AppBar(
+        title: Text('I am Poor'),
+        backgroundColor: Colors.deepOrangeAccent[100],
+      ),
+      body: Center(
+        child: Image(
+          image: AssetImage('images/IAMPOOR1.png'),
+        ),
+      ),
+    );
+  }
+}
